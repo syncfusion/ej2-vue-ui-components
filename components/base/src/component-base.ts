syncfusion/ej2-vue-ui-components

commit 08dac8fc4af9c9a92621e37923fc7268f9f35de3

v20.3.48 is released

components/base/src/component-base.ts
@@ -267,15 +267,12 @@ import Vue from 'vue';
                        tempObj[key] = childSlot.props[key];
                    });
                  }
-                 if ((/[s]\b/).test(tagRef) && innerDirValues) {
-                    if (!(/[s]\b/).test(tagName) || innerDirValues.length) {
-                        items[tagName] = tempObj;
-                    } else {
-                        items[tagName].push(tempObj);
-                    }
-                } else {
+                 if (((/[s]\b/).test(tagRef) && innerDirValues) && (!(/[s]\b/).test(tagName) || innerDirValues.length)) {
+                    items[tagName] = tempObj;
+                 }
+                 else if (tempObj && Object.keys(tempObj).length !== 0) {
                     items[tagName].push(tempObj);
-                }
+                 }
               }
              return items;
       }

commit b5cc929491ddab10067b3a5700f1f93ebb6530a2

base: fixing error introduced by 20.3.48
Fix #91 
Fixes a bug introduced in commit 08dac8fc4af9c9a92621e37923fc7268f9f35de3

More details here: https://www.syncfusion.com/forums/178147/version-20-3-48-give-typeerror-object-prototype-may-only-be-an-object-or-null-undefined-at?reply=SfrLo5

components/base/src/component-base.ts
@@ -269,8 +269,7 @@ import Vue from 'vue';
                  }
                  if (((/[s]\b/).test(tagRef) && innerDirValues) && (!(/[s]\b/).test(tagName) || innerDirValues.length)) {
                     items[tagName] = tempObj;
-                 }
-                 else if (tempObj && Object.keys(tempObj).length !== 0) {
+                 } else (tempObj && Object.keys(tempObj).length !== 0) {
                     items[tagName].push(tempObj);
                  }
               }